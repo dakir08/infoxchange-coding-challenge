07826fb069714d4b8d9b12548b316e55c63bf565
dakir08 <minhphuongsony@gmail.com>
2021-01-15 15:15:35 +1100

    setup environment with parcel

---
+++ client/src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export interface AppProps {}
+
+export const App: React.FunctionComponent<AppProps> = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
+};
