1065d5b23f94ebaf0c100de140e30d443a56c223
dakir08 <minhphuongsony@gmail.com>
2021-01-17 11:24:32 +1100

    cleaning code

--- client/src/Routes/Detail/Detail.style.ts
+++ client/src/Routes/Detail/Detail.style.ts
@@ -1,4 +1,3 @@
-import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import BookCoverImage from "../../../public/book_placeholder.png";
 import { StyledButton } from "../../shared/Button";
